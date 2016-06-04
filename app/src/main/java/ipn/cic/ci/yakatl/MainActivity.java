d4abaa7027008abac51f917fd5509835dc6050fd
monserratrh <rhdezmonserrat@gmail.com>
2016-06-03 19:11:18 -0500

    CardViwes

--- app/src/main/java/ipn/cic/ci/yakatl/MainActivity.java
+++ app/src/main/java/ipn/cic/ci/yakatl/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -21,29 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
-
-        // Mandar a llamar el api ejemplo
-        List<Sensor> sensor1 = RestApi.consultarWebServiceSensor(1,0);
-        List<Sensor> sensor2 = RestApi.consultarWebServiceSensor(2,0);
-        List<Sensor> sensor3 = RestApi.consultarWebServiceSensor(2,0);
-
-        for(int i = 0; i< sensor1.size(); i++) {
-                String[] arreglo = {
-                        sensor1.get(i).getId_wasp(),
-                        sensor1.get(i).getSensor(),
-                        sensor1.get(i).getValue(),
-                        sensor1.get(i).getTimestamp()
-                };
-
-                Toast.makeText(this,arreglo[0], Toast.LENGTH_SHORT).show();
-                Toast.makeText(this,arreglo[1], Toast.LENGTH_SHORT).show();
-                Toast.makeText(this,arreglo[2], Toast.LENGTH_SHORT).show();
-                Toast.makeText(this,arreglo[3], Toast.LENGTH_SHORT).show();
-
-                break;
-            }
+        consultarSensores();
 
         }
+
+    public void consultarSensores() {
+        TextView sensores = (TextView) findViewById(R.id.nivelSensores);
+        String aux = "\n" +String.valueOf(leerSensor(1)) + "\n" + String.valueOf(leerSensor(2)) + "\n" + String.valueOf(leerSensor(3) + "\n");
+        sensores.setText(aux);
+
+        /*MODIFICAR */
+
+        TextView calidad = (TextView)findViewById(R.id.calidad);
+        calidad.setText("\nLa calidad del aire es: \n BUENA\n");
+
+        /*MODIFICAR */
+    }
+
+    public char[] leerSensor(int numSensor)
+    {
+        List<Sensor> sensor = RestApi.consultarWebServiceSensor(numSensor,0);
+        int i = sensor.size()-1;
+        return ((sensor.get(i).getSensor() + ": "+ sensor.get(i).getValue()).toCharArray());
+    }
 
 
     @Override
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                Toast.makeText(getApplicationContext(), "REFRESH", Toast.LENGTH_SHORT).show();
+                consultarSensores();
                 return true;
             case R.id.action_share_fb:
                 Toast.makeText(getApplicationContext(), "SHARE ON FB", Toast.LENGTH_SHORT).show();
